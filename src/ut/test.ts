8e57449ed9142fe4944a38feb684cd539f67177f
Xie Yuheng <xyheme@gmail.com>
2021-10-08 06:48:22 +0800

    up

--- src/ut/test.ts
+++ src/ut/test.ts
@@ -6,12 +6,10 @@ export function test(description: string, fn: () => Promise<void>): void {
     .then(() => {
       const head = ut.colors.blue("Ok")
       console.log(`${head}: ${description}`)
-      process.exit()
     })
     .catch((error) => {
       const head = ut.colors.red("Fail")
       console.error(`${head}: ${description}`)
       console.error(error)
-      process.exit(1)
     })
 }
