b4b0918c8d09877f8dce9a2720506a2bfac3d27b
Xie Yuheng <xyheme@gmail.com>
2021-10-10 09:15:24 +0800

    - `FileStore.all` avoid using `await` in for Loop

--- src/file-store/file-store.ts
+++ src/file-store/file-store.ts
@@ -4,11 +4,10 @@ export abstract class FileStore extends Store<string> {
   abstract keys(): Promise<Array<string>>
 
   async all(): Promise<Record<string, string>> {
-    const files: Record<string, string> = {}
-    for (const path of await this.keys()) {
-      files[path] = await this.getOrFail(path)
-    }
-
-    return files
+    const keys = await this.keys()
+    const entries = await Promise.all(
+      keys.map((path) => [path, this.getOrFail(path)])
+    )
+    return Object.fromEntries(entries)
   }
 }
