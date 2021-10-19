97e8753c316f86e0aa1eaa75f7d3071f8d96f7ae
Xie Yuheng <xyheme@gmail.com>
2021-10-19 16:55:58 +0800

    - `FakeGitFileStore`

---
+++ src/git-file-stores/fake-git-file-store/fake-git-file-store.ts
@@ -0,0 +1,35 @@
+import { GitFileStore } from "../../git-file-store"
+
+export class FakeGitFileStore extends GitFileStore {
+  faked: Record<string, string>
+  fallback: GitFileStore
+
+  constructor(opts: {
+    fallback: GitFileStore
+    faked?: Record<string, string>
+  }) {
+    const { path, dir } = opts.fallback
+    super({ path, dir })
+    this.faked = opts.faked || {}
+    this.fallback = opts.fallback
+  }
+
+  async keys(): Promise<Array<string>> {
+    return Array.from(
+      new Set([...(await this.fallback.keys()), ...Object.keys(this.faked)])
+    )
+  }
+
+  async get(path: string): Promise<string | undefined> {
+    const found = this.faked[path]
+    if (found !== undefined) {
+      return found
+    } else {
+      return await this.fallback.get(path)
+    }
+  }
+
+  cd(subdir: string): GitFileStore {
+    return this.cd(subdir)
+  }
+}
