30ddbc983fdbea794113c1844254a541bbc1911c
Xie Yuheng <xyheme@gmail.com>
2021-12-13 01:44:43 +0800

    up

--- src/ut/slug.ts
+++ src/ut/slug.ts
@@ -2,7 +2,7 @@ export function slug(text: string): string {
   return text
     .trim()
     .toLowerCase()
-    .replace(/[^\p{Letter}\-_ ]+/gu, "")
+    .replace(/[^\p{Letter}\-_ 0-9]+/gu, "")
     .replace(/\s/gu, "-")
     .replace(/_/g, "-")
     .replace(/\-+/g, "-")
