a8c912cb08e5e423238f459dc304197b2cf3b19f
Xie Yuheng <xyheme@gmail.com>
2022-03-13 18:01:28 +0800

    up

--- src/ut/slug.test.ts
+++ src/ut/slug.test.ts
@@ -4,10 +4,10 @@ const target = "構造-the-constructivization-of-mathematics"
 
 // prettier-ignore
 {
-  ut.assertEqual(target, ut.slug("構造 / The constructivization of mathematics"))
-  ut.assertEqual(target, ut.slug("[構造] / The constructivization of mathematics---"))
-  ut.assertEqual(target, ut.slug("---[構造] / The constructivization of mathematics---"))
-  ut.assertEqual(target, ut.slug("---「構造」 / The constructivization of mathematics---"))
-  ut.assertEqual(target, ut.slug("---「構造」 / The constructivization of mathematics___"))
-  ut.assertEqual(target, ut.slug("---「構造」 / The_constructivization_of_mathematics___"))
+  ut.assertEquals(target, ut.slug("構造 / The constructivization of mathematics"))
+  ut.assertEquals(target, ut.slug("[構造] / The constructivization of mathematics---"))
+  ut.assertEquals(target, ut.slug("---[構造] / The constructivization of mathematics---"))
+  ut.assertEquals(target, ut.slug("---「構造」 / The constructivization of mathematics---"))
+  ut.assertEquals(target, ut.slug("---「構造」 / The constructivization of mathematics___"))
+  ut.assertEquals(target, ut.slug("---「構造」 / The_constructivization_of_mathematics___"))
 }
